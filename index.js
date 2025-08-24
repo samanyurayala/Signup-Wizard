8cbfa3b7d808d5d0bef2d0d5748df4ef493543cf
ItsGetWreckt <samanyu.rayala@gmail.com>
2025-08-24 11:33:07 -0700

    Added a birthday

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let value = document.getElementById("value");
 let text = document.getElementById("text");
+let spin = document.getElementById("spin");
+let date = document.getElementById("date");
 let letter_selector_first = `<select name="letters" class="letter_first">
         <option value=""></option>
         <option value="A">A</option>
@@ -66,3 +68,9 @@ letter_first.forEach(
 letter.forEach((element) => { element.innerHTML = letter_selector; });
 
 value.addEventListener("input", function() { text.textContent = value.value; });
+spin.addEventListener("click", function() {
+    let month_value = Math.floor(Math.random() * 12) + 1;
+    let day_value = Math.floor(Math.random() * 31) + 1;
+    let year_value = Math.floor(Math.random() * 56) + 1970;
+    date.textContent = month_value + " / " + day_value + " / " + year_value;
+});
